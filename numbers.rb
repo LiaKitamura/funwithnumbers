f7001467aba2ff5ac120d0f2603b641f3fcc57a2
Lia Kitamura <kit4mura@gmail.com>
2014-05-23 14:28:53 -0600

    added more calculator options

--- numbers.rb
+++ numbers.rb
@@ -1,9 +1,28 @@
 puts "Give me a number"
-num1 = gets.chomp
+num1 = gets.to_i
 
 puts "now give me another number"
-num2 = gets.chomp
+num2 = gets.to_i
 
-add = #{num1} + #{num2}
+puts "You chose #{num1} & #{num2} what would you like to do? add, subtract, divide or multiply?"
+calc = gets.chomp
 
-puts "your result is #{add}"
+div = num1/num2
+
+case calc
+when "subtract" || "Subtract"
+    puts num1 - num2
+  when "divide" || "Divide"
+    if div.eql?(div)
+      puts num1.to_f/num2.to_f
+    else
+      puts "#{div}.to_i"
+    end
+    #turn to string and search of .
+  when "multiply" || "Multiply"
+      puts num1 * num2
+  when "add" || "Add"
+      puts num1 + num2
+    else
+      puts "#{calc} not a valid entry. calculator shutting down"
+end
